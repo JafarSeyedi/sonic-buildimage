caf5bd79dbbbcd52c9bb6d5f54e73e5bcd61b2ca
JafarSeyedi <sjsayedi@yahoo.com>
2021-06-28 06:27:33 +0430

    disable broadcom-dnx

--- platform/broadcom/rules.mk
+++ platform/broadcom/rules.mk
@@ -21,8 +21,8 @@ include $(PLATFORM_PATH)/docker-saiserver-brcm.mk
 include $(PLATFORM_PATH)/one-image.mk
 include $(PLATFORM_PATH)/raw-image.mk
 include $(PLATFORM_PATH)/libsaithrift-dev.mk
-include $(PLATFORM_PATH)/docker-syncd-brcm-dnx.mk
-include $(PLATFORM_PATH)/docker-syncd-brcm-dnx-rpc.mk
+# include $(PLATFORM_PATH)/docker-syncd-brcm-dnx.mk
+# include $(PLATFORM_PATH)/docker-syncd-brcm-dnx-rpc.mk
 
 BCMCMD = bcmcmd
 $(BCMCMD)_URL = "https://sonicstorage.blob.core.windows.net/packages/20190307/bcmcmd?sv=2015-04-05&sr=b&sig=sUdbU7oVbh5exbXXHVL5TDFBTWDDBASHeJ8Cp0B0TIc%3D&se=2038-05-06T22%3A34%3A19Z&sp=r"
